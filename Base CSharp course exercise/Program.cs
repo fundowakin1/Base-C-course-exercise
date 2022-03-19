d9be06ca4a0812a4dd4c607f08d7b698777c4cc9
fundowakin <fundowakin@gmail.com>
2022-03-19 17:05:07 +0200

    Added text file for the task

--- Base CSharp course exercise/Program.cs
+++ Base CSharp course exercise/Program.cs
@@ -2,11 +2,23 @@
 
 namespace Base_CSharp_course_exercise
 {
+    /*
+     It is necessary to create an application that accepts the path to a text 
+     file as input and performs splitting the text into words.
+     All found words are formed into a dictionary that contains all 
+     occurrences of the word in the text and the positions of these occurrences.
+     After processing the text, the program should display statistics 
+     on the words found and the frequency of their repetitions in the text, 
+     the list should be sorted in descending order.
+     After displaying general statistics, the program waits for the user
+     to enter a word and returns information about all occurrences of the word 
+     in the text (line number and position).
+     */
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var text = ""
         }
     }
 }
