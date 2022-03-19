3680cf018e1bccef2efc4993561a4e9814f256e1
fundowakin <fundowakin@gmail.com>
2022-03-19 16:52:13 +0200

    Created solution; Added gitignore

---
+++ Base CSharp course exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Base_CSharp_course_exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
